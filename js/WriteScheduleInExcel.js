94416f3e1bcdf3f595cdebcdb787a589d2c906f0
ZT1996 <294238532@qq.com>
2018-07-25 09:56:59 +0800

    version1.0

--- js/WriteScheduleInExcel.js
+++ js/WriteScheduleInExcel.js
@@ -1,16 +1,26 @@
 function WriteScheduleExcel() {
-    let oHtml = document.getElementById('table').outerHTML;
-    let excelHtml = `
+    let inputDom = document.getElementById('totalList');
+    if (inputDom.value === '' || inputDom.value < 0) {
+        alert('请输入需要学习的单元!');
+    }
+    else {
+        let oHtml = document.getElementById('table').outerHTML;
+        let excelHtml = `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <style>
-        #table,#title{
-            display: none;
-            height: 50px;
-            font-size: 24px;
-            text-align: center;
-        }
+         #table {
+        display: none;
+        height: 50px;
+        font-size: 12px;
+        text-align: center;
+    }
+    #title{
+        display: none;
+        font-size: 20px;
+        text-align: center;
+    }
     </style>
 </head>
 <body>
@@ -18,10 +28,12 @@ ${oHtml}
 </body>
 </html>
 `;
-    let excelBlob = new Blob([excelHtml], {type: 'application/vnd.ms-excel'});
-    let oA = document.createElement('a');
-    oA.href = URL.createObjectURL(excelBlob);
-    oA.download = '艾宾浩斯记忆表.xls';
-    oA.click();
+        let excelBlob = new Blob([excelHtml], {type: 'application/vnd.ms-excel'});
+        let oA = document.createElement('a');
+        oA.href = URL.createObjectURL(excelBlob);
+        oA.download = '艾宾浩斯记忆表.xls';
+        oA.click();
+    }
+
 }
 
